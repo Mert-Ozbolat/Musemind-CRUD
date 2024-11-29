456ea36d9297bf21d4251d39d9337deb1fe09c61
Mert-Ozbolat <mertozbolat008@gmail.com>
2024-11-29 23:11:55 +0300

    first commit

---
+++ src/theme/color.js
@@ -0,0 +1,12 @@
+const ThemeColors = {
+    yellow: '#f68854',
+    blue: '#9d9bff',
+    pink: '#eda7ff',
+    lightPink: '#ffafaf',
+    green: '#5dd669',
+    white: '#fff',
+    black: '#181818',
+    gray: '#dddddd',
+}
+
+export { ThemeColors }
